3763518a5ad7333982d111004fecc19bbd279cfa
Danyang Song <ds6118@nyu.edu>
2020-07-24 19:28:43 -0400

    Delete UNIQUE attribute for User.password and User.phone

--- src/myModels.py
+++ src/myModels.py
@@ -8,8 +8,8 @@ class User(db.Model):
   __table_args__ = { 'extend_existing': True }
   uid = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(30), unique=True, nullable=False)
-  password = db.Column(db.String(120), unique=True, nullable=False)
-  phone = db.Column(db.String(30), unique=True, nullable=False)
+  password = db.Column(db.String(120), nullable=False)
+  phone = db.Column(db.String(30), nullable=False)
   ifAdmin = db.Column(db.Boolean, nullable=False, default=False)
 
   def __repr__(self):
